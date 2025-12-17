311b26bc2cddf051dbf6df00702d3c7709643414
Christopher Lee Crader <ccrader@uw.edu>
2025-12-17 09:56:10 -0800

    Add Disable-Sleep module

---
+++ Disable-Sleep.psm1
@@ -0,0 +1,23 @@
+# Disabled sleep functionality on Windows systems.
+# Christopher Lee Crader - University of Washington - ccrader@uw.edu
+# Taken from https://stackoverflow.com/questions/33790965/set-a-one-line-powershell-for-sleep-and-hibernate-to-turn-off
+Import-Module .\Get-Administrator.psm1
+
+function Disable-Sleep {
+	<#
+		.SYNOPSIS
+		Disable all sleep modes, including hibernation, display sleep, and disk timeout.
+	#>
+	if (Get-Administrator) {
+		powercfg.exe -x -monitor-timeout-ac 0
+		powercfg.exe -x -monitor-timeout-dc 0
+		powercfg.exe -x -disk-timeout-ac 0
+		powercfg.exe -x -disk-timeout-dc 0
+		powercfg.exe -x -standby-timeout-ac 0
+		powercfg.exe -x -standby-timeout-dc 0
+		powercfg.exe -x -hibernate-timeout-ac 0
+		powercfg.exe -x -hibernate-timeout-dc 0
+	} else {
+		Write-Output "Administrator required to disable sleep modes."
+	}
+}
--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 Just a collection of Powershell scripts I find useful for various projects.
 
 ## Contents
+- Disable-Sleep.psm1 - Disables sleep modes (sleep, hibernate, disk, monitor). Requires Admin.
 - Get-Administrator.psm1 - Returns whether the current shell is Admin or not as a T/F.
 - PSHelpers.psm1 - All of the helper functions in one module. Just imports the others.
 - Read-YesNo.psm1 - Prompts a Y/N question and returns a T/F value (Yes is True, No is False)
